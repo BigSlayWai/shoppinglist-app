97830ae60717b6326fa7b0236fc65c4bee986afa
Wai Hleng Swe <waihlengpyae@gmail.com>
2025-05-09 23:41:19 +0100

    hello

--- src/firebase.js
+++ src/firebase.js
@@ -17,13 +17,12 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAhuUmknCme9dTA44d-jw93NHs16bQrnUk",
-    authDomain: "shopping-list-app-459315.firebaseapp.com",
-    projectId: "shopping-list-app-459315",
-    storageBucket: "shopping-list-app-459315.firebasestorage.app",
-    messagingSenderId: "744209796510",
-    appId: "1:744209796510:web:ca0c5894beef7159c99a8f",
-    measurementId: "G-R6WG0K7WN6"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
   };
 
 
